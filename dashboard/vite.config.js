c69bf76c4e41a9f078f9f8ac365d979947d133ba
elkatmisfurkan <elkatmisfurkan65@gmail.com>
2026-05-18 18:46:47 +0300

    4

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BeeVoice-IoT/'
+  base: '/BeeVoice-IoT/',
+  build: {
+    outDir: 'docs'
+  }
 })
